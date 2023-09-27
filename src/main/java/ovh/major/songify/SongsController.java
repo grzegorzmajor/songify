9e275537923b8391d21fcbba5a047fe886d7b860
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-09-27 18:32:56 +0200

    added POST method in controller

--- src/main/java/ovh/major/songify/SongsController.java
+++ src/main/java/ovh/major/songify/SongsController.java
@@ -1,11 +1,14 @@
 package ovh.major.songify;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -13,7 +16,7 @@ import java.util.stream.Collectors;
 @Log4j2
 class SongsController {
 
-    Map<Integer, String> simpleDatabase = new HashMap<>(Map.of(
+    Map<Integer, String> simpleSongsDatabase = new HashMap<>(Map.of(
             1, "Wlazł kotek na płotek",
             2, "Z popielnika na Wojtusia iskiereczka mruga",
             3, "Ach śpij Kochanie",
@@ -25,14 +28,14 @@ class SongsController {
     @GetMapping("/songs")
     public ResponseEntity<SongResponseDto> getSongs(@RequestParam(required = false) Integer limit) {
         if (limit != null) {
-            Map<Integer, String> limitedMap = simpleDatabase.entrySet()
+            Map<Integer, String> limitedMap = simpleSongsDatabase.entrySet()
                     .stream()
                     .limit(limit)
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
             return ResponseEntity.ok(new SongResponseDto(limitedMap));
         }
 
-        SongResponseDto response = new SongResponseDto(simpleDatabase);
+        SongResponseDto response = new SongResponseDto(simpleSongsDatabase);
         return ResponseEntity.ok(response);
     }
 
@@ -41,13 +44,49 @@ class SongsController {
             @PathVariable(name = "id") Integer songId,
             @RequestHeader(required = false) String requestId) {
         log.info("RequestId: " + requestId);
-        String song = simpleDatabase.get(songId);
+        String song = simpleSongsDatabase.get(songId);
         if (song == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         SingleSongResponseDto response = new SingleSongResponseDto(song);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/songs")
+    public ResponseEntity<List<SingleSongResponseDto>> addSongs(@RequestBody @NotBlank List<SingleSongRequestDto> songs) {
+        if (songs.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        List<SingleSongResponseDto> response = new ArrayList<>();
+
+        songs.forEach(
+                song -> {
+                    int newKey = newKey(simpleSongsDatabase);
+                    if (song.songName().length() > 0) {
+                        simpleSongsDatabase.put(newKey, song.songName());
+                        log.info("Added song named: " + song.songName() + " with id: " + newKey);
+                        response.add(new SingleSongResponseDto(song.songName()));
+                    } else {
+                        log.info("Song skipped - empty name");
+                    }
+                });
+        return ResponseEntity.ok(response);
+    }
+
+    private Integer newKey(Map<Integer, String> map) {
+        Integer lastKey = lastKey(map);
+        int newKey = 1;
+        if (lastKey != null) {
+            newKey = lastKey + 1;
+        }
+        return newKey;
+    }
+    private Integer lastKey(Map<Integer, String> map) {
+        Integer lastKey = null;
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            lastKey = entry.getKey();
+        }
+        return lastKey;    }
 
 }
 
