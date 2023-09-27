f46cf8500e32fcdf843b7c1b04384438ee7c0041
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-09-27 14:30:08 +0200

    Initial commit

--- src/test/java/ovh/major/songify/SongifyApplicationTests.java
+++ src/test/java/ovh/major/songify/SongifyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SongifyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
