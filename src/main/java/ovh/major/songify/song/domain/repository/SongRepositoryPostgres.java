7cfaa621180e462ed23b8ed29adff3c17419aeab
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-09-30 11:27:40 +0200

    added new method

--- src/main/java/ovh/major/songify/song/domain/repository/SongRepositoryPostgres.java
+++ src/main/java/ovh/major/songify/song/domain/repository/SongRepositoryPostgres.java
@@ -1,5 +1,7 @@
 package ovh.major.songify.song.domain.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import ovh.major.songify.song.domain.model.SongEntity;
 
@@ -12,5 +14,11 @@ public interface SongRepositoryPostgres extends Repository<SongEntity, Long> {
     SongEntity save(SongEntity song);
     List<SongEntity> findAll();
     Optional<SongEntity> findById(Long id);
+    void deleteById(Long id);
+    boolean existsById(Long id);
+
+    @Modifying
+    @Query("UPDATE SongEntity s SET s.name = :#{#song.name}, s.artist = :#{#song.artist} WHERE s.id = :id")
+    SongEntity updateById(Long id, SongEntity song);
 
 }
